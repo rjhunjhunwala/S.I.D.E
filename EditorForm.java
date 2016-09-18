8161cd4fa21cf4a119804c267b6d4f59addc759e
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-09-18 01:55:58 -0400

    split screen output connected, added tab support

--- EditorForm.java
+++ EditorForm.java
@@ -1,7 +1,7 @@
-import java.awt.Font;
-import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JFrame;
 
 /**
  * @author PhiNotPi
@@ -18,106 +18,29 @@ public class EditorForm extends Form {
     this.width = width;
   }
 
-  List<Character> curLine() {
-    return lines.get(curLineNum);
+  @Override
+  void clear() {
+    List<List<Character>> lines = new ArrayList<List<Character>>();
+    lines.add(new ArrayList<Character>());
+    curLineNum = 0;
+    curColNum = 0;
+    histColNum = 0;
   }
 
-  public String toString() {
-    String res = "";
-    // res += curLineNum + " " + curColNum + " " + histColNum + "\n";
-    for (int l = 0; l < lines.size(); l++) {
-      if (l > 0) {
-        res += "\n";
-      }
-      for (int c = 0; c < lines.get(l).size(); c++) {
-        res += lines.get(l).get(c);
-      }
-    }
-    return res;
+  @Override
+  void addProc(Process proc, JFrame f) {
+    // TODO Auto-generated method stub
+
   }
 
-  // public String[] formatLines() {
-  // ArrayList<String> res = new ArrayList<String>();
-  //
-  // return (String[]) res.toArray();
-  // }
-
-  void applyFormInput(String s) {
-    for (Character pushed : s.toCharArray()) {
-      applyFormInput(pushed);
-    }
+  @Override
+  boolean canBackspace() {
+    return true;
   }
 
-  void applyFormInput(char pushed) {
-    if ((int) pushed == 8) {
-      if (curColNum == 0 && curLineNum != 0) {
-        int newColNum = lines.get(curLineNum - 1).size();
-        lines.get(curLineNum - 1).addAll(curLine());
-        lines.remove(curLineNum);
-        curLineNum--;
-        curColNum = newColNum;
-      } else if (curColNum > 0) {
-        curLine().remove(curColNum - 1);
-        curColNum--;
-      }
-    } else if ((int) pushed == 127) {
-      // add delete functionality
-    } else if ((int) pushed == 10) {
-      List<Character> newLine = curLine().subList(curColNum, curLine().size());
-      lines.add(curLineNum + 1, new ArrayList<Character>(newLine));
-      newLine.clear();
-      curLineNum++;
-      curColNum = 0;
-    } else if ((int) pushed >= 32) {
-      curLine().add(curColNum, pushed);
-      curColNum++;
-    } else {
-      curLine().add(curColNum, '?');
-      curColNum++;
-    }
-    histColNum = curColNum;
-  }
-
-  void cursorLeft() {
-    if (curColNum == 0 && curLineNum != 0) {
-      curLineNum--;
-      curColNum = curLine().size();
-    } else if (curColNum > 0) {
-      curColNum--;
-    }
-    histColNum = curColNum;
-  }
-
-  void cursorRight() {
-    if (curColNum == curLine().size() && curLineNum < lines.size() - 1) {
-      curLineNum++;
-      curColNum = 0;
-    } else if (curColNum < curLine().size()) {
-      curColNum++;
-    }
-    histColNum = curColNum;
-  }
-
-  void cursorUp() {
-    if (curLineNum > 0) {
-      curLineNum--;
-      if (histColNum > curLine().size()) {
-        curColNum = curLine().size();
-      } else {
-        curColNum = histColNum;
-      }
-    }
-  }
-
-  void cursorDown() {
-    if (curLineNum < lines.size() - 1) {
-      curLineNum++;
-      if (histColNum > curLine().size()) {
-        curColNum = curLine().size();
-      } else {
-        curColNum = histColNum;
-      }
-    }
+  @Override
+  boolean canInsert() {
+    return true;
   }
 
 }
