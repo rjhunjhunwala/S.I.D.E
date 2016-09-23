4935d752b41ed6d8ffeee5a204c8c8ada1093d74
PhiNotPi <PhiNotPi@users.noreply.github.com>
2016-09-23 19:31:54 -0400

    Add files via upload

--- IDE/Language.java
+++ IDE/Language.java
@@ -7,5 +7,9 @@ public abstract class Language {
   abstract protected boolean isValidSyntax(String source);
 
   abstract protected ArrayList<String> splitTokens(String source);
+  
+  abstract protected String getLangInfo();
+  
+  protected abstract Language clone();
 
 }
